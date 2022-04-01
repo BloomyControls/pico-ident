e100ddba3e86f291bc91735ce356945ee73b9c18
Will Eccles <william.r.eccles@bloomy.com>
2022-04-01 12:22:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Raspberry Pi Pico System Identification
+
+This project uses a Raspberry Pi Pico to identify a system and store various
+information about the system, settable and retrievable over UART or USB.
+
+## Requirements
+
+You'll need Ubuntu or Debian to build this (WSL works just fine). Before
+attempting to build this project, install required packages using the following
+commands:
+
+```
+sudo apt update
+sudo apt upgrade
+sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential python3 git
+```
+
+## Building
+
+First, you must configure the build system:
+
+```
+cmake -S . -B build
+```
+
+This step may take quite some time, as it will download the Pi Pico SDK. If you
+wish to use USB for serial communications instead of UART, add `-DUSB_SERIAL=1`
+to the above command.
+
+After the configuration step is done, you can build the project like so:
+
+```
+cmake --build build
+```
+
+This will generate all the outputs in the build directory. The UF2 file (used to
+flash the pico) will be named `pico-ident.uf2`.
+
+## Installing
+
+To install the firmware onto the pico, hold down the BOOTSEL button on the pico
+while you are plugging in its USB cable. It should show up as a storage device
+in your file explorer. Simply drag and drop the UF2 file to it and it should
+unmount automatically. It's now flashed and ready to use.
---
+++ src/main.c
@@ -0,0 +1,169 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "hardware/flash.h"
+#include "hardware/sync.h"
+#include "pico/stdlib.h"
+#include "pico/unique_id.h"
+
+/*
+ * Offset 512K from the start of the flash.
+ * Must be aligned to a 4096-byte sector.
+ */
+#define FLASH_TARGET_OFFSET (512 * 1024)
+
+/*
+ * All writes must be a whole number of pages, so we'll use the page size (256)
+ * as our maximum length (or a multiple thereof).
+ */
+#define MAX_LEN (FLASH_PAGE_SIZE)
+
+/*
+ * This is the structure containing the info to store.
+ *
+ * Each of the strings in this structure is assumed to be null-terminated.
+ */
+struct device_info {
+  char mfg[64];
+  char name[64];
+  char ver[64];
+  char date[64];
+
+  char user1[64];
+  char user2[64];
+  char user3[64];
+  char user4[64];
+};
+
+// Size of the device info structure + additional space to make it a multiple of
+// the flash page size (all writes must be whole numbers of pages).
+#define DEVINFO_SIZE (((sizeof(struct device_info) / FLASH_PAGE_SIZE) +     \
+                       !!(sizeof(struct device_info) % FLASH_PAGE_SIZE)) *  \
+                       FLASH_PAGE_SIZE)
+
+// Device info in flash (read-only, you cannot write through this pointer).
+const struct device_info* flash_devinfo =
+    (const struct device_info*)(XIP_BASE + FLASH_TARGET_OFFSET);
+
+// Board ID (this is set only once and stored here).
+char board_id[PICO_UNIQUE_BOARD_ID_SIZE_BYTES * 2 + 1];
+
+void store_devinfo(const struct device_info* info) {
+  uint32_t ints = save_and_disable_interrupts();
+  flash_range_erase(FLASH_TARGET_OFFSET, FLASH_SECTOR_SIZE);
+  restore_interrupts(ints);
+
+  if (info != NULL) {
+    static uint8_t buf[DEVINFO_SIZE] = {0};
+    memcpy(buf, info, sizeof(*info));
+
+    ints = save_and_disable_interrupts();
+    flash_range_program(FLASH_TARGET_OFFSET, buf, DEVINFO_SIZE);
+    restore_interrupts(ints);
+  }
+}
+
+// handle a null-terminated message
+void handle_msg(char* msg) {
+  if (msg == NULL)
+    return;
+
+  static struct device_info wrinfo;
+
+  if (strncmp(msg, "MFG=", 4) == 0) {
+    msg += 4;
+    msg[strnlen(msg, 63)] = '\0';
+    wrinfo = *flash_devinfo;
+    strncpy(wrinfo.mfg, msg, 64);
+    store_devinfo(&wrinfo);
+  } else if (strncmp(msg, "MFG?", 4) == 0) {
+    printf("%s\n", flash_devinfo->mfg);
+  } else if (strncmp(msg, "NAME=", 5) == 0) {
+    msg += 5;
+    msg[strnlen(msg, 63)] = '\0';
+    wrinfo = *flash_devinfo;
+    strncpy(wrinfo.name, msg, 64);
+    store_devinfo(&wrinfo);
+  } else if (strncmp(msg, "NAME?", 5) == 0) {
+    printf("%s\n", flash_devinfo->name);
+  } else if (strncmp(msg, "VER=", 4) == 0) {
+    msg += 4;
+    msg[strnlen(msg, 63)] = '\0';
+    wrinfo = *flash_devinfo;
+    strncpy(wrinfo.ver, msg, 64);
+    store_devinfo(&wrinfo);
+  } else if (strncmp(msg, "VER?", 4) == 0) {
+    printf("%s\n", flash_devinfo->ver);
+  } else if (strncmp(msg, "DATE=", 5) == 0) {
+    msg += 5;
+    msg[strnlen(msg, 63)] = '\0';
+    wrinfo = *flash_devinfo;
+    strncpy(wrinfo.date, msg, 64);
+    store_devinfo(&wrinfo);
+  } else if (strncmp(msg, "DATE?", 5) == 0) {
+    printf("%s\n", flash_devinfo->date);
+  } else if (strncmp(msg, "SERIAL?", 7) == 0) {
+    printf("%s\n", board_id);
+  } else if (strncmp(msg, "USER1=", 6) == 0) {
+    msg += 6;
+    msg[strnlen(msg, 63)] = '\0';
+    wrinfo = *flash_devinfo;
+    strncpy(wrinfo.user1, msg, 64);
+    store_devinfo(&wrinfo);
+  } else if (strncmp(msg, "USER1?", 6) == 0) {
+    printf("%s\n", flash_devinfo->user1);
+  } else if (strncmp(msg, "USER2=", 6) == 0) {
+    msg += 6;
+    msg[strnlen(msg, 63)] = '\0';
+    wrinfo = *flash_devinfo;
+    strncpy(wrinfo.user2, msg, 64);
+    store_devinfo(&wrinfo);
+  } else if (strncmp(msg, "USER2?", 6) == 0) {
+    printf("%s\n", flash_devinfo->user2);
+  } else if (strncmp(msg, "USER3=", 6) == 0) {
+    msg += 6;
+    msg[strnlen(msg, 63)] = '\0';
+    wrinfo = *flash_devinfo;
+    strncpy(wrinfo.user3, msg, 64);
+    store_devinfo(&wrinfo);
+  } else if (strncmp(msg, "USER3?", 6) == 0) {
+    printf("%s\n", flash_devinfo->user3);
+  } else if (strncmp(msg, "USER4=", 6) == 0) {
+    msg += 6;
+    msg[strnlen(msg, 63)] = '\0';
+    wrinfo = *flash_devinfo;
+    strncpy(wrinfo.user4, msg, 64);
+    store_devinfo(&wrinfo);
+  } else if (strncmp(msg, "USER4?", 6) == 0) {
+    printf("%s\n", flash_devinfo->user4);
+  } else if (strncmp(msg, "CLEAR", 5) == 0) {
+    memset(&wrinfo, 0, sizeof(wrinfo));
+    store_devinfo(&wrinfo);
+  }
+}
+
+int main(void) {
+  stdio_init_all();
+
+  // Get the board ID (we only need to do this once)
+  pico_get_unique_board_id_string(board_id, sizeof(board_id));
+
+  char rdbuf[512] = {0};
+  size_t idx = 0;
+  int c;
+  while (1) {
+    c = getchar_timeout_us(10);
+    if (c == PICO_ERROR_TIMEOUT)
+      continue;
+
+    if (c == '\r') {
+      rdbuf[idx] = '\0';
+      idx = 0;
+      handle_msg(rdbuf);
+    } else if (isprint(c)) {
+      rdbuf[idx] = c;
+      idx = (idx + 1) % sizeof(rdbuf);
+    }
+  }
+}
